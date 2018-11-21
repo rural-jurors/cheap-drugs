ebb614b0e46511894215ab9d63345c97d753773f
Mihir Kothari <mkothari@homeaway.com>
2018-11-20 20:11:29 -0600

    wip

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,39 @@
-let request = require("request")
+let request = require("request-promise")
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
     // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-
     app.get("/api/drugs/:drugName&:zipCode", function (req, res) {
         let openFDAEndpoint = "https://api.fda.gov/drug/label.json";
-        request.get({url: openFDAEndpoint, qs:{"search": `description:${req.params.drugName}`}}, function(err, response){
-            res.json(JSON.parse(response.body));
-        })
+        let openFDASearch = {
+            "search": `description:${req.params.drugName}`
+        }
+        let nytEndpoint = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        let nytSearch = {
+            'api-key': "97b8ee4143194df68c8a1637095f6e03",
+            'q': req.params.drugName,
+            'sort': "newest"
+        };
+
+        Promise.all([request.get({
+                url: openFDAEndpoint,
+                qs: openFDASearch
+            }), request.get({
+                url: nytEndpoint,
+                qs: nytSearch
+            })])
+            .then(([openFDAResponse, nytResponse]) => {
+                let obj = {
+                    openFDA: JSON.parse(openFDAResponse),
+                    nyt: JSON.parse(nytResponse)
+                };
+                res.json(obj);
+            });
     });
 
     // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-
     app.post("/api/signup", function (req, res) {
         console.log(req.body);
         console.log("The user signed up!");

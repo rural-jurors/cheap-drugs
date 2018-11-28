85fa4a346f3d03c77270aced88419e2d73292302
SSeal <nniesel90@gmail.com>
2018-11-28 12:57:38 -0600

    Added photos fixed news ticker

--- public/js/news.js
+++ public/js/news.js
@@ -2,7 +2,7 @@
  * pulls information from the form and build the query URL
  * @returns {string} URL for NYT API based on form inputs
  */
-function buildQueryURL() {
+/* function buildQueryURL() {
     // queryURL is the url we'll use to query the API
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
@@ -142,9 +142,68 @@ var queryURL = buildQueryURL();
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(updatePage);
+}).then(updatePage); */
 
 /* $(function () {
     console.log("started liscroll");
     $("ul.list-group").liScroll();
 }); */
+
+$(".empty-list").empty();
+searchTerm = "pharmaceutical drug costs";
+console.log(searchTerm);
+numRecords = 10;
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url +=
+  "?" +
+  $.param({
+    "api-key": "bd0477f20cd244eea6a7564838882bda",
+    q: `${searchTerm}`,
+    page: `${numRecords}`
+  });
+$.ajax({
+  url: url,
+  method: "GET"
+})
+  .done(function(result) {
+    console.log(result);
+    for (let i = 0; i < numRecords; i++) {
+      $(".empty-list").append(
+        "<li><a href=" +
+          result.response.docs[i].web_url +
+          ">" +
+          result.response.docs[i].snippet +
+          "</a></li>"
+      );
+    }
+    scroller();
+  }
+)
+  .fail(function(err) {
+    throw err;
+  });
+
+//$("ul.empty-list").liScroll();
+
+
+
+    function scroller() {
+        
+      var scroll = $('div.scroll');// Sets the div with a class of scroll as a variable
+      
+      var height = scroll.height(); // Gets the height of the scroll div
+      
+      var topAdj = -height-30; /* '-height' turns the height                   of the UL into a negative #, 
+                   * '- 50' subtracts an extra 50 pixels from the height of 
+                         * the div so that it moves the trail of the UL higher to 
+                                 * the top of the div before the animation                ends
+                                 */
+        
+        scroll.animate({
+            'top' : [topAdj, 'linear'] 
+        }, 50000, function(){
+            scroll.css('top', 250); //resets the top position of the Ul for the next cycle
+            scroller(); // Recalls the animation cycle to begin.
+        });}
+        

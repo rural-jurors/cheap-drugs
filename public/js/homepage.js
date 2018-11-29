309e92a37de4a119ce89e009ed0747bfbe8de7bb
SSeal <nniesel90@gmail.com>
2018-11-29 02:05:36 -0600

    Displaying api results

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,19 +1,72 @@
-$( document ).ready(function() {
-    $(".dropdown-trigger").dropdown();
-
-    $(".submit").on("click", function(event) { 
-        var newSearch = {
-            prescription: $("#search").val().trim(),
-            zip: parseInt($("#zip").val().trim())
-        };
-        $.get("/api/drugs", newSearch, function(data){
-            var results = document.getElementById("results");
-            results.appendChild("<p> Description: "+data.openFDA.results.description+"</p>");
-            results.appendChild("<p> Warnings: "+data.openFDA.results.boxed_warnings+"</p>");
-            results.appendChild("<p> Generic Name: "+data.openFDA.results.generic_name+"</p>");
-            results.appendChild("<p> Manufacturer Name: "+data.openFDA.results.manufacturer_name+"</p>");
-            results.appendChild("<p> Route: "+data.openFDA.results.route+"</p>");
-        })
-    
-    });
+$(document).ready(function() {
+  $(".dropdown-trigger").dropdown();
 });
+
+$("#searchBtn").on("click", function(event) {
+  console.log("Submit was clicked");
+  var newSearch = $("#search").val().trim()+"&"+$("#zip").val().trim();
+
+  console.log(newSearch);
+
+  //var pName = document.getElementById("prescription-name");
+  //pName.innerHTML(newSearch.drugName);
+
+  $.get("/api/drugs/" + newSearch, function(data) {
+    var mName = document.getElementById("manufacturer-name");
+    var gName = document.getElementById("generic-name");
+    var descrip = document.getElementById("description");
+    var warns = document.getElementById("warnings");
+    var pRoute = document.getElementById("route");
+
+    console.log(data);
+
+    descrip.innerHTML = data.openFDA.results[0].description;
+    warns.innerHTML = data.openFDA.results[0].boxed_warnings;
+    gName.innerHTML = data.openFDA.results[0].generic_name;
+    mName.innerHTML = data.openFDA.results[0].manufacturer_name;
+    pRoute.innerHTML = data.openFDA.results[0].route;
+
+    $(".empty-list").empty();
+
+    for (let i = 0; i < 10; i++) {
+        $(".empty-list").append(
+          "<li><a href=" +
+            data.nyt.response.docs[i].web_url +
+            ">" +
+            data.nyt.response.docs[i].snippet +
+            "</a></li>"
+        );
+    }
+
+    scroller();
+
+  });
+});
+
+
+
+    
+    
+function scroller() {
+  var scroll = $("div.scroll"); // Sets the div with a class of scroll as a variable
+
+  var height = scroll.height(); // Gets the height of the scroll div
+
+  var topAdj = -height - 30;
+  /* '-height' turns the height                   of the UL into a negative #,
+   * '- 50' subtracts an extra 50 pixels from the height of
+   * the div so that it moves the trail of the UL higher to
+   * the top of the div before the animation                ends
+   */
+
+  scroll.animate(
+    {
+      top: [topAdj, "linear"]
+    },
+    50000,
+    function() {
+      scroll.css("top", 250); //resets the top position of the Ul for the next cycle
+      scroller(); // Recalls the animation cycle to begin.
+    }
+  );
+}
